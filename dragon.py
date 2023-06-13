bb0797a8425736f0619a0fca97573271d69cbe63
dave <concave@acm.org>
2023-06-12 20:00:50 -0600

    game files

--- dragon.py
+++ dragon.py
@@ -1,5 +1,19 @@
 """Module to represent a Dragon in the game"""
 
-# class Dragon:
-#
-#     def
+
+class Dragon:
+
+    def __init__(self, game):
+        self.image = None
+        self.name = ''
+        self.color = ''
+
+        self.screen = game.screen
+
+        # Start each piece off in the top left corner.
+        self.x, self.y = 0.0, 0.0
+
+    def blitme(self):
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.x, self.y
+        self.screen.blit(self.image, self.rect)
